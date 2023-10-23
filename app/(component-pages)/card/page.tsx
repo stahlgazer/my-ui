d0ecb4d1a059f86e465b958eac50bcee01ff7e7d
Gavin S. <stahlgavin@gmail.com>
2023-10-23 16:18:44 -0700

    card effects done

--- app/(component-pages)/card/page.tsx
+++ app/(component-pages)/card/page.tsx
@@ -1,22 +1,18 @@
 export default function Card() {
   return (
-    <div className="w-[250px] rounded-md border-2 border-fuchsia-500 bg-black m-auto">
-      <div className="border-b border-fuchsia-500 p-4">
+    <div className="w-[250px] rounded-md border-2 border-fuchsia-500 bg-black m-auto shadow-2xl shadow-fuchsia-500">
+      <div className="border-b border-dotted border-fuchsia-500 p-4">
         <h2 className="text-2xl font-bold underline decoration-fuchsia-500 ">
           Card Title
         </h2>
       </div>
       <div className="p-4 text-sm">
-        <h4 className="text-base font-semibold underline decoration-fuchsia-500">Card Sub-Title</h4>
-        <p>
-          Some Cool Stuff
-        </p>
-        <p>
-          More Cool Stuff
-        </p>
-        <p>
-          Coolest of all Stuff
-        </p>
+        <h4 className="text-base font-semibold underline decoration-fuchsia-500">
+          Card Sub-Title
+        </h4>
+        <p>Some Cool Stuff</p>
+        <p>More Cool Stuff</p>
+        <p>Coolest of all Stuff</p>
       </div>
     </div>
   );
