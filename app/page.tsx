cd6a795e4890d58346b976a36983ece6a0d33e12
Gavin S. <stahlgavin@gmail.com>
2023-10-22 21:25:29 -0700

    styling landing page

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,13 @@ export default function Home() {
         height={250}
         className="animate-bounce"
       />
-      <h1>
-        Amazingly Simple Components for React using TailwindCSS.
-        <br></br>
-        Choose Between Your Favorite Themes
+      <h1 className="text-2xl font-bold">
+        Amazingly Simple Components for React using TailwindCSS.{" "}
       </h1>
+      <p className="font-semibold bg-gradient-to-r from-emerald-500 via-fuchsia-500 to-sky-500 py-2 px-4 rounded-md mt-6">
+        Choose Between Your Favorite Themes
+      </p>
+
       <Link href="/introduction">
         <button className="animate-background-shine transition ease-in-out m-8 border-2 border-fuchsia-500 rounded-lg p-2 hover:border-white hover:text-fuchsia-500 hover:ease-linear">
           Documentation
