f0ae44744f26627f4bec85bcbee6bae3028a6dfd
Gavin S. <stahlgavin@gmail.com>
2023-10-23 14:25:06 -0700

    loading

---
+++ app/(component-pages)/loading/page.tsx
@@ -0,0 +1,7 @@
+import { LoaderIcon } from "lucide-react";
+
+export default function Loader() {
+  return (
+    <LoaderIcon className="animate-spin text-fuchsia-500 h-6 w-6 lg:h-11 lg:w-11 m-auto border rounded-full" />
+  );
+}
