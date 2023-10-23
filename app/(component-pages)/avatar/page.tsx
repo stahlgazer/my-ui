c89b6ab98b3158387f779327f35435c3aca6fb33
Gavin S. <stahlgavin@gmail.com>
2023-10-23 15:06:43 -0700

    avatar

--- app/(component-pages)/avatar/page.tsx
+++ app/(component-pages)/avatar/page.tsx
@@ -2,13 +2,12 @@ import avatar from "@/app/avatar.jpg";
 import Image from "next/image";
 export default function Avatar() {
   return (
+    // change Image to img for user to copy paste
     <div>
       <Image
         alt="my-ui logo"
         src={avatar}
-        width={100}
-        height={100}
-        className={`rounded-full border-2 border-white bg-cover bg-center transition-all duration-300 hover:translate-x-[2px] hover:translate-y-[2px] m-auto`}
+        className={`w-[100px] h-[auto] rounded-full border-2 border-white bg-cover bg-center m-auto`}
       />
     </div>
   );
