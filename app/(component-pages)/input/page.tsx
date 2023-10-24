5251e315b46f18d56474ef266c256f9ca27ec40b
Gavin S. <stahlgavin@gmail.com>
2023-10-23 17:39:56 -0700

    input

--- app/(component-pages)/input/page.tsx
+++ app/(component-pages)/input/page.tsx
@@ -1,5 +1,19 @@
-export default function Input() {
-    return <div>Input</div>;
-  };
+"use client";
+import { useState } from "react";
 
-  
+export default function Input() {
+  const [value, setValue] = useState("");
+  return (
+    <div>
+      <input
+        className="rounded-md border-2 text-fuchsia-500 border-fuchsia-500 p-[10px] focus:ring focus:ring-fuchsia-500"
+        type="text"
+        placeholder={`Talk to me...`}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
